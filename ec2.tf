f60a4b72964fc5071347b57f2fa1e45aab394e91
CodingManoj <mvp2612@hotmail.com>
2023-03-05 20:11:31 -0500

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -12,3 +12,24 @@ resource "aws_spot_instance_request" "spot-server" {
   }
 }
 
+
+# Installing RabbitMQ
+
+resource "null_resource" "app" {
+
+  count   = var.OD_INSTANCE_COUNT + var.SPOT_INSTANCE_COUNT
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = local.SSH_USER
+      password = local.SSH_PASSWORD
+      host     = element(local.ALL_INSTANCE_IPS, count.index)  
+      }
+
+      inline = [
+          "ansible-pull -U https://github.com/b52-clouddevops/ansible.git -e MONGO_URL=${data.terraform_remote_state.db.outputs.MONGO_ENDPOINT} -e COMPONENT=${var.COMPONENT} -e DB_PASSWORD=RoboShop@1 -e ENV=${var.ENV} -e APP_VERSION=${var.APP_VERSION} -e ENV=dev robot-pull.yml"
+        ]
+    }
+
+}
