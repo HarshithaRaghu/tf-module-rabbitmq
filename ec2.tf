2329ba527c5d27d2e1a66de8e5d9ff20cb0a8bba
CodingManoj <mvp2612@hotmail.com>
2023-03-05 20:09:44 -0500

    Auto Push Commit

--- ec2.tf
+++ ec2.tf
@@ -3,11 +3,11 @@ resource "aws_spot_instance_request" "spot-server" {
   ami                     = data.aws_ami.lab-image.id
   instance_type           = "t3.micro"
   subnet_id               = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS[0]
-  vpc_security_group_ids  = [aws_security_group.allow_app.id]
+  vpc_security_group_ids  = [aws_security_group.allow_rabbit.id]
   wait_for_fulfillment    = true
   iam_instance_profile    = "b52-admin-role"
 
   tags = {
-    Name = "${var.COMPONENT}-${var.ENV}"
+    Name = "rabbitmq-${var.ENV}"
   }
 }
