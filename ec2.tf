09908991bafbb1dee606aa3f528dd8c5f663a8fe
CodingManoj <mvp2612@hotmail.com>
2023-03-05 20:09:03 -0500

    Auto Push Commit

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+# Creates SPOT Servers
+resource "aws_spot_instance_request" "spot-server" {
+  ami                     = data.aws_ami.lab-image.id
+  instance_type           = "t3.micro"
+  subnet_id               = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+  vpc_security_group_ids  = [aws_security_group.allow_app.id]
+  wait_for_fulfillment    = true
+  iam_instance_profile    = "b52-admin-role"
+
+  tags = {
+    Name = "${var.COMPONENT}-${var.ENV}"
+  }
+}
