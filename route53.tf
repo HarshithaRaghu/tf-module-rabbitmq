7c580556864f9d298d390c592ce3b8eef62f2309
CodingManoj <mvp2612@hotmail.com>
2023-03-05 20:17:41 -0500

    Auto Push Commit

--- route53.tf
+++ route53.tf
@@ -1,7 +1,7 @@
 resource "aws_route53_record" "dns-record" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_ID
-  name    = "${var.COMPONENT}-dev.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}" : "${var.COMPONENT}-dev.${data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTEDZONE_NAME}"
-  type    = "CNAME"
+  name    = "${var.COMPONENT}-dev.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTEDZONE_NAME}"
+  type    = "A"
   ttl     = 10
-  records = var.ALB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
+  records =  [aws_sp] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
 }
